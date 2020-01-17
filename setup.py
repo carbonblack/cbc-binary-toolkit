32ea47b49978799c78b86c879392528ebf81aee7
Alex Van Brunt <avanbrunt@carbonblack.com>
2020-01-17 15:04:20 -0700

    Add base files

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Python setup script for PIP packaging"""
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """Process files for configuration"""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="cb-binary-analysis",
+    version=read("VERSION"),
+    url="https://developer.carbonblack.com/",
+    license="MIT",
+    author="Carbon Black",
+    author_email="dev-support@carbonblack.com",
+    description="Carbon Black Binary Analysis",
+    long_description=__doc__,
+    platforms="any",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=read("requirements.txt").splitlines(),
+    tests_require=read("tests/requirements.txt").splitlines()
+)
