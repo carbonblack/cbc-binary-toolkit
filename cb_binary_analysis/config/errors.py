37c99e43fa5df5e8448607eea480d0ec0970c969
Amy Bowersox <abowersox@vmware.com>
2020-01-23 14:18:00 -0700

    CBAPI-1285: added integration to main, unit tests
    Also de-flake8'd the code.

--- cb_binary_analysis/config/errors.py
+++ cb_binary_analysis/config/errors.py
@@ -6,11 +6,19 @@ Binary analysis sdk for managing and submitting hashes
 Exceptions raised by configuration code.
 """
 
+
 class ConfigError(Exception):
+    """
+    Exception thrown in the event of errors in the configuration.
+    """
     def __init__(self, message=None, original_exception=None):
         self.message = str(message)
         self.original_exception = original_exception
-        
+
     def __str__(self):
+        """
+        Return the message associated with the exception
+
+        :return str:
+        """
         return self.message
-    
--- cb_binary_analysis/config/model.py
+++ cb_binary_analysis/config/model.py
@@ -10,13 +10,20 @@ from .errors import ConfigError
 
 
 class Config:
+    default_location = 'config/binary-analysis-config.yaml'
     _required_id = 'cb-binary-analysis'
-    
+
     def __init__(self, data):
         self._data = data
-        
+
     @classmethod
     def load(cls, data):
+        """
+        Load YAML data into a Config object.
+
+        :param data: Data to be loaded. May be a string, or an open text or binary stream.
+        :return: The Config object that contains the data in the config file.
+        """
         try:
             mydata = yaml.safe_load(data)
             if isinstance(mydata, dict):
@@ -34,13 +41,27 @@ class Config:
                 mark = exc.problem_mark
                 message = message + (' at (%s,%s)' % (mark.line + 1, mark.column + 1))
             raise ConfigError(message, exc)
-        
+
     @classmethod
     def load_file(cls, filename):
+        """
+        Load a YAML file into a Config object.
+
+        :param filename: The name of the file to be loaded.
+        :return: The Config object that contains the data in the config file.
+        """
         with open(filename, 'r') as file:
             return Config.load(file)
 
     def _seek_path(self, path, suppress_exceptions=False):
+        """
+        Seeks out a value in the configuration data with a specific path.
+
+        :param path: The path to the configuration variable (with components separated by '.')
+        :param suppress_exceptions: If this is true, None will be returned for paths that do
+            not exist. If this is False, a ConfigError will be raided in that circumstance.
+        :return: The configuration value, which may be of any type.
+        """
         cur = None
         elt = None
         for s in path.split('.'):
@@ -56,12 +77,26 @@ class Config:
         return cur[elt]
 
     def string(self, path):
+        """
+        Returns a string configuration value from the configuration data.
+
+        :param path: The path to the configuration variable (with components separated by '.')
+        :return: The value.
+        """
         v = self._seek_path(path)
         if isinstance(v, str):
             return v
         raise ConfigError('value not string type: ' + path)
 
     def section(self, path):
+        """
+        Returns a sub-section of the configuration data as another Config object. This accesses the same
+        data, but without the prefix. For example, if B == A.section("foo"), then
+        B.string("bar") == A.string("foo.bar").
+
+        :param path: The path to the configuration section (with components separated by '.')
+        :return: The subsection as a new Config object.
+        """
         v = self._seek_path(path)
         if isinstance(v, dict):
             return Config(v)
