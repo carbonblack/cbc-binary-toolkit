84a010a9174ba981fdab4cc3711009ad23ce015b
Luke Lyon <llyon@carbonblack.com>
2020-02-05 13:08:48 -0700

    Add CBAPI code

--- cb_binary_analysis/ubs.py
+++ cb_binary_analysis/ubs.py
@@ -1,122 +1,122 @@
 # -*- coding: utf-8 -*-
 
+
 """
 Functions to retrieve binaries from UBS
 """
 
-from dataclasses import dataclass
-#from main import config
-import json
-import requests
+from cbapi.psc.threathunter.models import Binary, Downloads
+from cbapi.psc.threathunter import CbThreatHunterAPI
+from config.model import Config
 import logging
-
+from time import sleep
 
 log = logging.getLogger(__name__)
 
 
-@dataclass
-class Binary:
+def _create_cbth(args):
+    """Generates a CbThreatHunterAPI object to use in other functions"""
+    cbth = CbThreatHunterAPI(url=args['url'], token=args['apitoken'], ssl_verify=args['ssl_verify'], org_key=args['orgkey'])
+    return cbth
+
+
+def _download_hashes(cbth, hashes, expiration_seconds=60):
+    """Download hashes from UBS"""
+    try:
+        log.debug("Downloading hashes from UBS")
+        downloads = Downloads(cbth, hashes, expiration_seconds)
+        return downloads
+    except Exception as err:
+        log.error(f"Error downloading hashes from UBS: {err}")
+        return
+
+
+def _download_hash_metadata(cbth, downloads):
+    """Retrieve metadata for each found hash"""
+    metadata_list = []
+    try:
+        log.debug("Downloading metadata information")
+        for download in downloads:
+            binary_metadata = download._info
+            th_binary = cbth.select(Binary, download.sha256)
+            if isinstance(th_binary, Binary):
+                binary_metadata.update(th_binary._info)
+
+            metadata_list.append(binary_metadata)
+        return metadata_list
+    except Exception as err:
+        log.error(f"Error downloading hash metadata from UBS: {err}")
+
+    return
+
+
+def _retry_download(cbth, hashes_with_download_errors, attempt_num):
+    """Retries to download any hashes that errored out while downloading."""
+    downloaded_hashes = []
+    print(f"Attempt {attempt_num} to re-download {hashes_with_download_errors}.")
+    if attempt_num > 5:
+        log.debug("Reached retry limit for downloading hashes that experienced an error during download")
+    else:
+        downloaded_hashes = _download_hashes(cbth, hashes_with_download_errors)
+    return downloaded_hashes, attempt_num + 1
+
+
+def _check_download(cbth, download, attempt_num):
     """
-    Represents a retrievable binary.
+    Verifies the presence of Downloads.FoundItem. Retries downloading
+    if there are errors during download.
     """
-    sha256: str
-    url: str
+    if not download:
+        log.error("No hashes were found in the Universal Binary Store.")
+        return
+
+    if download.not_found:
+        log.warning(f"{len(download.not_found)} hashes were not found in the Universal Binary Store: {download.not_found}")
+
+    # if download.found and attempt_num == 1:  # download.error:
+    if download.error:
+        log.warning(f"{len(download.error)} hashes experienced an error while downloading: {download.error}. Retrying download.")
+
+        retry, attempt = _retry_download(cbth, download.error, attempt_num)
+        # retry, attempt = _retry_download(cbth, ["0995f71c34f613207bc39ed4fcc1bbbee396a543fa1739656f7ddf70419309fc"], attempt_num)
+        sleep(2)
+
+        retry_check = _check_download(cbth, retry, attempt)
+
+        if retry_check:
+            return download, retry
+        # this needs to be tested
+
+    if download:
+        return download
 
 
-class Downloads(Binary):
-    """
-    Represents download information for a list of process hashes.
-    """
+def download(hashes):
+    """Initiates download of hashes and their metadata"""
+    config = Config.load_file('../config/binary-analysis-config.yaml')
 
-    class FoundItem(Binary):
-        """
-        Represents the download URL and process hash for a successfully
-        located binary.
-        """
-        def __init__(self, item):
-            super(Downloads.FoundItem, self).__init__(item["sha256"], item["url"])
+    cbth = _create_cbth(config._data['carbonblackcloud'])
 
-    class NotFoundItem(Binary):
-        """
-        Represents the process hash for an unsuccessfully
-        located binary.
-        """
-        def __init__(self, item):
-            super(Downloads.NotFoundItem, self).__init__(item, None)
+    downloaded_hashes = _download_hashes(cbth, hashes)
 
-    class Errored(Binary):
-        """
-        Represents the process hash for an unsuccessfully
-        located binary.
-        """
-        def __init__(self, item):
-            super(Downloads.Errored, self).__init__(item, None)
+    found_hashes = _check_download(cbth, downloaded_hashes, attempt_num=1)
 
-    def __init__(self, sha_dict):
-        self.body = json.dumps(sha_dict)
-        self.headers = {'Content-type': 'application/json',
-                        'X-Auth-Token': '8NZIDZMHIFW4QLAFC52ZZCFH/BI61IM2YSW'
-                        }
+    metadata = []
 
-        #url = f'{config.url}/ubs/v1/orgs/{config.org_key}/file/_download'
-        #self.token = config.token
-        #self.token_header = {'X-Auth-Token': self.token, 'User-Agent': None}
+    if isinstance(found_hashes, tuple):
+        log.debug("Successfully re-downloaded hashes that errored out during download.")
+        for item in found_hashes:
+            try:
+                hash_metadata = _download_hash_metadata(cbth, item.found)
+                metadata.append(hash_metadata[0])
+            except Exception as err:
+                log.error(f"Failed to download metadata for {item.found[0].sha256}: {err}")
 
-        self.myurl = 'https://defense-eap01.conferdeploy.net'
-        self.myorg_key = 'WNEXFKQ7'
-        self.url = f'{self.myurl}/ubs/v1/orgs/{self.myorg_key}/file/_download'
-        self.session = requests.Session()
-        self.r = self.session.request("POST", self.url, headers=self.headers,
-                                      data=self.body).json()
-
-    def download(self, shas, expiration_seconds=3600):
-        """
-        Can be used to re-download hashes that had an issue during
-        initial download
-        """
-        body = {
-            "sha256": shas,
-            "expiration_seconds": expiration_seconds
-        }
-        r = self.session.request("POST", self.url, headers=self.headers,
-                                 data=body).json()
-        return r
-
-    @property
-    def found(self):
-        """
-        Returns a list of Downloads.FoundItem, one
-        for each binary found in the binary store.
-        """
-        return [Downloads.FoundItem(item) for item in self.r['found']]
-
-    @property
-    def not_found(self):
-        """
-        Returns a list of Downloads.NotFoundItem, one
-        for each binary not found in the binary store.
-        """
-        return [Downloads.NotFoundItem(item) for item in self.r['not_found']]
-
-    @property
-    def errored_out(self):
-        """
-        Returns a list of Downloads.Errored, one
-        for each binary submitted to the UBS that
-        had an intermittent error.
-        """
-        return [Downloads.Errored(item) for item in self.r['error']]
+    else:
+        # log.debug("No hashes errored out while downloading.")
+        metadata.append(_download_hash_metadata(cbth, found_hashes.found)[0])
+    print(f"len {len(metadata)} Metadata from input: {metadata}")
+    return metadata
 
 
-sha_dict = {"sha256": ["6c4eb3c9e0f478b2d19a329687d113ba92c90a17d0caa6c40247a5afff31f0cd", "0995f71c34f613207bc39ed4fcc1bbbee396a543fa1739656f7ddf70419309fc"], "expiration_seconds": 3600 }
-download_0 = Downloads(sha_dict)
-found = download_0.found
-print(found)
-not_found = download_0.not_found
-print(not_found)
-errored = download_0.errored_out
-print(errored)
-
-if errored:
-    retry = download_0.download(shas=[item["sha256"] for item in download_0.errored_out])
-    print(retry)
+download(["6c4eb3c9e0f478b2d19a329687d113ba92c90a17d0caa6c40247a5afff31f0cd", "0995f71c34f613207bc39ed4fcc1bbbee396a543fa1739656f7ddf70419309fc"])
