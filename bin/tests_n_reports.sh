c02b71c749ac11754b9f8c9d13e4085ce332e3bc
bvasil-cb <bvasil@carbonblack.com>
2020-02-11 10:06:15 -0700

    fix minor typo in services to have coveralls to work

--- bin/tests_n_reports.sh
+++ bin/tests_n_reports.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
+
 set -e
+
+echo 'Running tests....'
 coverage run -m pytest
+
+echo 'Running report and sending to coveralls....'
 coverage report -m
-# coveralls  => Currently working on this
+coveralls
