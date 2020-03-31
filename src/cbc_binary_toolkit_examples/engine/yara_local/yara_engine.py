966f797ecaf9cdd27647f139674f57236b8e7974
Alex Van Brunt <avanbrunt@carbonblack.com>
2020-03-31 15:49:47 -0600

    Remove threading from LocalEngineManager and Yara example

--- src/cbc_binary_toolkit_examples/engine/yara_local/yara_engine.py
+++ src/cbc_binary_toolkit_examples/engine/yara_local/yara_engine.py
@@ -7,7 +7,6 @@ import requests
 import uuid
 import yara
 
-from threading import Thread
 from cbc_binary_toolkit import InitializationError
 from cbc_binary_toolkit.engine import LocalEngineFactory
 log = logging.getLogger(__name__)
@@ -15,21 +14,17 @@ log = logging.getLogger(__name__)
 
 class YaraFactory(LocalEngineFactory):
     """Yara Factory"""
-    def create_engine(self, config, pub_sub_manager):
+    def create_engine(self, config):
         """Creates yara engine threads"""
-        return YaraEngine(kwargs={"config": config, "pub_sub_manager": pub_sub_manager})
+        return YaraEngine(config)
 
 
-class YaraEngine(Thread):
+class YaraEngine():
     """Local yara engine"""
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None):
+    def __init__(self, config):
         """Yara engine thread, pulling from engine pub/sub queue"""
-        super(YaraEngine, self).__init__(group=group, target=target, name=name)
         self.name = "Yara"
-        self.config = kwargs.get("config", None)
-        self.pub_sub_manager = kwargs.get("pub_sub_manager", None)
-        self.result_queue_name = self.config.get("pubsub.result_queue_name")
+        self.config = config
 
         if self.config.get("engine.name") != self.name:
             log.error("Attempted to init engine with non matching engine config")
@@ -38,14 +33,21 @@ class YaraEngine(Thread):
         # Compile yara rules
         self.rules = yara.compile(self.config.get("engine.rules_dir"))
 
-    def run(self):
-        """Start of thread"""
-        while True:
-            binary_metadata = self.pub_sub_manager.get(self.name)
+    def analyze(self, binary_metadata):
+        """
+        Analyze the binary
 
-            if binary_metadata is None:
-                log.debug("Exiting YaraEngine Thread")
-                break
+        Args:
+            binary_metadata (dict): The binary metadata to be analyzed
+
+        Returns:
+            EngineResponseSchema: Results from analzying the binary
+
+        """
+        result = None
+        if not isinstance(binary_metadata, dict):
+            log.error(f"Recieved unexpected input: {type(binary_metadata)}")
+        else:
             try:
                 resp = requests.get(binary_metadata["url"], stream=True)
                 resp.raise_for_status()
@@ -68,17 +70,21 @@ class YaraEngine(Thread):
                         "severity": highest_severity
                     })
 
-                self.pub_sub_manager.put(self.result_queue_name, {
+                result = {
                     "iocs": iocs,
                     "engine_name": self.name,
                     "binary_hash": binary_metadata["sha256"],
                     "success": True
-                })
+                }
             except Exception as e:
                 log.error(f"Failed processing binary: {e}")
-                self.pub_sub_manager.put(self.result_queue_name, {
-                    "iocs": [],
-                    "engine_name": self.name,
-                    "binary_hash": binary_metadata["sha256"],
-                    "success": False
-                })
+
+        if result is None:
+            return {
+                "iocs": [],
+                "engine_name": self.name,
+                "binary_hash": binary_metadata.get(["sha256"], None) if isinstance(binary_metadata, dict) else None,
+                "success": False
+            }
+        else:
+            return result
