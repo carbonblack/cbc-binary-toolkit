95a45d006cdab703008d5e4eaba3997f760055e7
Alex Van Brunt <avanbrunt@carbonblack.com>
2020-04-08 15:13:45 -0600

    Add schema validation for engine_manager. Add yara test

--- src/tests/component/test_engine_manager.py
+++ src/tests/component/test_engine_manager.py
@@ -10,6 +10,7 @@ from cbc_binary_toolkit.engine import LocalEngineManager
 from cbc_binary_toolkit.schemas import EngineResponseSchema
 
 from tests.component.engine_fixtures.mock_engine import MockLocalEngine
+from tests.component.schema_fixtures.mock_data import VALID_BINARY_METADATA, MISSING_FIELDS_BINARY_METADATA
 
 ENGINE_NAME = "MockEngine"
 
@@ -41,7 +42,21 @@ def test_analyze(config):
     """Test analyze pass through"""
     manager = LocalEngineManager(config)
 
-    assert EngineResponseSchema.validate(manager.analyze({"sha256": "TEST_HASH"}))
+    assert EngineResponseSchema.validate(manager.analyze(VALID_BINARY_METADATA))
+
+
+@pytest.mark.parametrize("input", [
+    MISSING_FIELDS_BINARY_METADATA,
+    {}
+])
+def test_analyze_invalid_schema(config, input):
+    """Test analyze pass through"""
+    manager = LocalEngineManager(config)
+
+    result = manager.analyze(input)
+    if result["binary_hash"] is not None:
+        result = EngineResponseSchema.validate(result)
+    assert not result["success"]
 
 
 @pytest.mark.parametrize("engine_config, exception", [
