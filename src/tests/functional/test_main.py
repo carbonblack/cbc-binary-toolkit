33e60b67629f0758a7451cce4d3e2d2ad2775c66
Luke Lyon <llyon@carbonblack.com>
2020-06-19 12:25:17 -0600

    add config tests to func test suite

--- src/tests/functional/test_main.py
+++ src/tests/functional/test_main.py
@@ -66,6 +66,28 @@ engine:
     """)
 
 
+def _format_invalid_config(auth_token, feed_id):
+    """Configure for invalid test cases in this module."""
+    return (f"""id: cbc_binary_toolkit
+version: 0.0.1
+carbonblackcloud:
+  url: https://defense-eap01.conferdeploy.net
+  api_token: {auth_token}
+  org_key: WNEXFKQ7
+  ssl_verify: True
+  expiration_seconds: 3600
+database:
+  _provider: cbc_binary_toolkit.state.builtin.Persistor
+  location: ":memory:"
+engine:
+  name:
+  feed_id: {feed_id}
+  type: local
+  _provider: cbc_binary_toolkit_examples.engine.yara_local.yara_engine.YaraFactory
+  rules_file: __file__/example_rule.yara
+    """)
+
+
 def _create_feed(auth_token):
     """Create a Feed to use for testing"""
     # Feed Creation
@@ -90,6 +112,17 @@ def create_and_write_config(auth_token):
     feed_id = feed_info["id"]
     with open("config/functional_config.yml", "w") as f:
         config_text = _format_config(auth_token, feed_id)
+        f.write(config_text)
+    return feed_id
+
+
+@pytest.fixture()
+def create_and_write_invalid_config(auth_token):
+    """Generate and write the test config to file"""
+    feed_info = _create_feed(auth_token)
+    feed_id = feed_info["id"]
+    with open("config/functional_config.yml", "w") as f:
+        config_text = _format_invalid_config(auth_token, feed_id)
         f.write(config_text)
     return feed_id
 
@@ -206,3 +239,26 @@ class TestUserHandling:
             assert len(result['iocs_v2']) == num_hashes
         assert num_reports == 2
         delete_feed(auth_token, create_and_write_config)
+
+    def test_invalid_configuration(self, create_and_write_invalid_config, auth_token):
+        """Test running cbc-binary-analysis with invalid config"""
+        with open(LOG_FILE, "a+") as log:
+            subprocess.call(['cbc-binary-analysis', '-c', 'config/nonexistant_file',
+                             'analyze', '-l', '["405f03534be8b45185695f68deb47d4daf04dcd6df9d351ca6831d3721b1efc4"]'],
+                            stdout=log, stderr=log)
+
+        with open(LOG_FILE, "r") as log:
+            assert log.readlines()[-2].strip() == ("FileNotFoundError: [Errno 2] No such"
+                                                   " file or directory: 'config/nonexistant_file'")
+        delete_feed(auth_token, create_and_write_invalid_config)
+
+    def test_invalid_configuration_1(self, create_and_write_invalid_config, auth_token):
+        """Test running cbc-binary-analysis with invalid config"""
+        with open(LOG_FILE, "a+") as log:
+            subprocess.call(['cbc-binary-analysis', '-c', 'config/functional_config.yml',
+                             'analyze', '-l', '["405f03534be8b45185695f68deb47d4daf04dcd6df9d351ca6831d3721b1efc4"]'],
+                            stdout=log, stderr=log)
+        with open(LOG_FILE, "r") as log:
+            assert log.readlines()[-1].strip() == ("ERROR:cbc_binary_toolkit_examples.tools.analysis_util:Failed"
+                                                   " to create Local Engine Manager. Check your configuration")
+        delete_feed(auth_token, create_and_write_invalid_config)
