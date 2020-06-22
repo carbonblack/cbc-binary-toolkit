cb2818f8e9b695d4aae212bb32f04ad8e141de9e
Amy Bowersox <abowersox@vmware.com>
2020-06-22 15:54:52 -0600

    functional tests don't need code coverage information

--- bin/func_tests.bat
+++ bin/func_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo "Running functional tests..."
-coverage run -m pytest src\tests\functional\test_main.py --token %CBC_AUTH_TOKEN%
+pytest src\tests\functional\test_main.py --token %CBC_AUTH_TOKEN%
