4633a962502684af3abcbc71b0f38ea305cff31e
Alex Van Brunt <avanbrunt@carbonblack.com>
2020-06-24 15:59:09 -0600

    Modify functional test script to exit and upload on test failure. Renable steps

--- bin/codeship_func_tests.sh
+++ bin/codeship_func_tests.sh
@@ -3,4 +3,11 @@
 echo "Running functional tests..."
 pytest src/tests/functional/test_main.py --token $CBC_AUTH_TOKEN
 
-aws s3 cp /app/src/tests/functional/log.txt s3://binarytoolkit-functional-tests/$CI_TIMESTAMP.$1.log.txt
+if [ $? -eq 0 ]
+then
+  exit 0
+else
+  echo "Pushing log file to S3..."
+  aws s3 cp /app/src/tests/functional/log.txt s3://binarytoolkit-functional-tests/$CI_COMMIT_ID.$1.log.txt
+  exit 1
+fi
