254e7d3c27a45883026f8252540f302f6dba1468
Luke Lyon <llyon@carbonblack.com>
2020-06-26 09:51:32 -0600

    exclude load tests in steps

--- bin/tests_n_reports.bat
+++ bin/tests_n_reports.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo "Running tests..."
-coverage run -m pytest --ignore=src\tests\functional
+coverage run -m pytest --ignore=src\tests\functional  --ignore=src\tests\load
 
 echo "Running report and sending to coveralls...."
 coverage report -m
--- bin/tests_n_reports.sh
+++ bin/tests_n_reports.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo 'Running tests....'
-coverage run -m pytest --ignore=src/tests/functional
+coverage run -m pytest --ignore=src/tests/functional --ignore=src/tests/load
 
 echo 'Running report and sending to coveralls....'
 coverage report -m
